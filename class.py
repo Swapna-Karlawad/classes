33e7c67a6d7ed015662d4730740cb095b6496cf4
swapna-karlawad <swapnakarlawad@gmail.com>
2025-10-30 15:25:59 +0530

    save to eligibility-check

--- class.py
+++ class.py
@@ -1,4 +1,8 @@
-classes_held=int(input("Enter the total number of classes held."))
-classes_attended=int(input("Enter the total number of classes attended."))
-attendance_percentage=(classes_attended/classes_held)*100
-print("Attendance percentage:",attendance_percentage,"%")
+classes_held = int(input("Enter the total number of classes held: "))
+classes_attended = int(input("Enter the number of classes attended: "))
+attendance_percentage = (classes_attended / classes_held) * 100
+print("Attendance Percentage:", attendance_percentage, "%")
+if attendance_percentage >= 75:
+    print("You are allowed to write the exam.")
+else:
+    print("You are not allowed to write the exam.")
