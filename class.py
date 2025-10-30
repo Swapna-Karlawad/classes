7c3c1c52ffad93b7e100b57ecb73cbc658fbda77
swapna-karlawad <swapnakarlawad@gmail.com>
2025-10-30 15:19:35 +0530

    save

---
+++ class.py
@@ -0,0 +1,4 @@
+classes_held=int(input("Enter the total number of classes held."))
+classes_attended=int(input("Enter the total number of classes attended."))
+attendance_percentage=(classes_attended/classes_held)*100
+print("Attendance percentage:",attendance_percentage,"%")
